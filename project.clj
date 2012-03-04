a5d83335c2264d4aea411cc73914f6fa470ba608
Ivan Willig <iwillig@gmail.com>
2012-03-04 16:32:02 -0500

    Updated the version of hiccup we are using.

--- project.clj
+++ project.clj
@@ -1,16 +1,19 @@
 (defproject solutions-dashboard "1.0.0-SNAPSHOT"
   :description "FIXME: write description"  
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [hiccup "0.3.8"]
+                 [hiccup "1.0.0-beta1"]
                  [clj-http "0.3.2"]
                  [rmarianski/ring-jetty-servlet-adapter "0.0.2"]
+
                  [compojure "1.0.1"]
+                 [flutter "0.0.7"]
                  [org.quartz-scheduler/quartz "2.1.2"]
                  [org.slf4j/slf4j-api "1.6.4"]
                  [org.slf4j/slf4j-simple "1.6.4"]
                  [com.draines/postal "1.7-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.clojure/data.json "0.1.2"]
+
                  [postgresql "9.0-801.jdbc4"]]
   :dev-dependencies [[swank-clojure "1.4.0"]
                      [lein-ring "0.5.4"]]
