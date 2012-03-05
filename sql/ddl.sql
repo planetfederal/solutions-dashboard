aadf75f90bbcb35688ec03ea55c323b9d8354e53
Ivan Willig <iwillig@gmail.com>
2012-03-04 19:24:03 -0500

    Make the employee table suck less.

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,8 +3,7 @@ drop table if exists employees;
 
 create table employees (
        id              serial primary key,
-       first_name      varchar(255),
-       last_name       varchar(255),
+       name            varchar(255),
        trello_username varchar(255),
        email           varchar(255),
        add_at          timestamp DEFAULT current_timestamp
