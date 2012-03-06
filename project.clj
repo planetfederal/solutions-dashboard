07598069583f194f4198543cc755a5979cf89664
Ivan Willig <iwillig@gmail.com>
2012-03-06 07:48:32 -0500

    Added json params to the project file.

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
                  [hiccup "1.0.0-beta1"]
                  [clj-http "0.3.2"]
                  [rmarianski/ring-jetty-servlet-adapter "0.0.2"]
-
+                 [ring-json-params "0.1.3"]
                  [compojure "1.0.1"]
-                 [flutter "0.0.7"]
                  [clj-decline "0.0.5"]
 
                  [org.quartz-scheduler/quartz "2.1.2"]
