af8d79a88732ec1531ff59412b0a81c055f62a19
Ivan Willig <iwillig@gmail.com>
2012-03-09 11:24:25 -0500

    Added an auth namespace.

---
+++ src/solutions_dashboard/auth.clj
@@ -0,0 +1,11 @@
+(ns solutions-dashboard.auth)
+
+
+(defn session-get-user [request]
+  (.getAttribute (:ses request) "user"))
+
+(defn session-save-user [request user]
+  (.setAttribute (:ses request) "user" user))
+
+(defn session-delete [request]
+  (.invalidate (:ses request)))
